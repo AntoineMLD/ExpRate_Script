28865ec5c945c6ccd0dd2a7f66426257d9ab5db2
AntoineMLD <antoine.moulard@hotmail.com>
2025-01-02 22:15:10 +0100

    modification for monster dungeon

--- exp_rate_hook.lua
+++ exp_rate_hook.lua
@@ -3,33 +3,32 @@ local function OnGiveXP(event, player, amount, victim)
     local rate = controller:GetRate(player)
 
 
-    local adjustedXP = amount
+    local adjustedXP = math.floor(amount * rate)
+    player:GiveXP(adjustedXP)
+
+
     if victim and victim:IsInDungeon() then
-        adjustedXP = math.floor(amount * rate)
         player:SendBroadcastMessage("You earned " .. adjustedXP .. " XP in a dungeon with a " .. rate .. "x rate!")
     else
-        adjustedXP = math.floor(amount * rate)
+        player:SendBroadcastMessage("You earned " .. adjustedXP .. " XP with a " .. rate .. "x rate!")
     end
-
-
-    player:GiveXP(adjustedXP)
 
     return false 
 end
-
 
 local function OnQuestReward(event, player, quest, xp)
     local controller = ExpRateController:GetInstance()
     local rate = controller:GetRate(player)
 
+
     local adjustedXP = math.floor(xp * rate)
     player:GiveXP(adjustedXP)
 
-
+    -- Message de confirmation
     player:SendBroadcastMessage("Quest completed! You earned " .. adjustedXP .. " XP with a " .. rate .. "x rate!")
     return false 
 end
 
--- Enregistrement des hooks
+
 RegisterPlayerEvent(12, OnGiveXP) 
 RegisterPlayerEvent(28, OnQuestReward) 
